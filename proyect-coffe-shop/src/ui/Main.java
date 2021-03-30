5ad27cc5126b4b111642a2ed13d9605517e1f07e
Amilcar Stebann <amilcarshm@gmail.com>
2021-03-29 20:04:55 -0500

    Main controller

--- proyect-coffe-shop/src/ui/Main.java
+++ proyect-coffe-shop/src/ui/Main.java
@@ -5,10 +5,18 @@
  */
 package ui;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  *
  * @author amilc
  */
-public class Main {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
     
 }
