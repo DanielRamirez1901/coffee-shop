2b986c7c21702f1679257795edf35101ece79364
Amilcar Stebann <amilcarshm@gmail.com>
2021-03-29 20:39:37 -0500

    Revert "prueba2"
    This reverts commit 0645235cf073c1e89da54c1c2cf933504bca7e88.

--- proyect-coffe-shop/src/ui/Main.java
+++ proyect-coffe-shop/src/ui/Main.java
@@ -1,17 +1,18 @@
+
 package ui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    
+/**
+ *
+ * @author amilc
+ */
+public class Main extends Application{
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
 
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
     
 }
