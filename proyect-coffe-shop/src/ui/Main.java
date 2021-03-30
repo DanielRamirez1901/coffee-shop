eebbc114da1e0ea8ec257ac8bdd8236fbad12dc1
Dani <danirg1414@outlook.com>
2021-03-29 21:24:07 -0500

    Creation of ui classes, creation of login interface

--- proyect-coffe-shop/src/ui/Main.java
+++ proyect-coffe-shop/src/ui/Main.java
@@ -1,18 +1,40 @@
 
 package ui;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Employee;
 
 /**
  *
  * @author amilc
  */
 public class Main extends Application{
+	
+	private LoginScreenController loginScreenController;
+	private Employee employee;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+	public Main() {
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("loginScreen.fxml"));
+
+		fxmlLoader.setController(loginScreenController);
+		
+		Parent root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Casa dorada");
+		primaryStage.show();
+	}
+
     
 }
